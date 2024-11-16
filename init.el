311777e943809a4742100213fb08d8343c581119
Maurizio Vitale <109253047+m-vitale@users.noreply.github.com>
2024-11-16 11:23:36 -0500

    Initial init.el (the neo Emacs distro) (#22)
    GitOrigin-RevId: 729f202efefa3a5f8d1fb5bb0f2b9a30fd1c2af4

---
+++ init.el
@@ -0,0 +1,9 @@
+;;; init.el --- Where the magic begins ✨
+
+;; This is the beginning of a beautiful config. 
+;; Abandon all hope, ye who enter uncustomized.
+
+;; Warning: Side effects may include productivity, editor superiority,
+;; and unsolicited evangelism about Emacs.
+
+;; Let there be `(setq)!`
